deb820f4ac596d9d7a743086bb0eca1590fda2b8
9marissaramirezespinoza-web <9marissaramirezespinoza@gmail.com>
2026-01-29 13:02:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -178,6 +178,7 @@ with st.sidebar:
 st.title("CONSUMOS Y RENDIMIENTOS 📈")
 tab1, tab2 = st.tabs(["Captura", "Dashboard"])
 
+with tab1:
 if st.session_state.guardado_ok:
     st.success("✅ Guardado correctamente en la base de datos.")
     if st.session_state.get("sheets_error"):
@@ -339,6 +340,7 @@ if st.button("GUARDAR✅"):
             st.rerun()
         except Exception as e:
             table_messages.error(f"❌ Error al guardar en TiDB: {e}")
+
 
 
 
